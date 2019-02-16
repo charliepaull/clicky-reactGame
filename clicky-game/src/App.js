6040dba2cff50dde46602b0ffde6ca223d65d70e
Charlie Paull <charliepaull211@gmail.com>
2019-02-16 11:18:08 -0800

    photos rendered! CSS not good, time to randomize.

--- clicky-game/src/App.js
+++ clicky-game/src/App.js
@@ -2,17 +2,27 @@ import React, {Component} from "react";
 import Nav from "./components/navBar/navBar";
 import Jumbo from "./components/jumbotron/jumbo";
 import Card from "./components/cardLayout/cardLayout";
+import Seinfeld from "./seinfeld.json";
 
 class App extends Component {
+
+    state = {
+        Seinfeld
+    };
+
     render() {
         return(
             <div>
-                <Nav />,
-                <Jumbo />,
-                <Card />
+                <Nav />
+                <Jumbo />
+            {this.state.Seinfeld.map(characters => (
+                <Card 
+                    seinfeldImg = {characters.image}
+                />
+            ))}
             </div>
-        )
-    }
-}
+        );
+    };
+};
 
 export default App;
--- clicky-game/src/components/cardLayout/cardLayout.js
+++ clicky-game/src/components/cardLayout/cardLayout.js
@@ -4,119 +4,12 @@ import "./cardLayout.css";
 function Card(props){
     return (
         <div>
-            <div className="row1-container">
-                <div className="card-group">
-                    <div className="card">
-                        <img src="..." className="card-img-top" alt="..."/>>
-                        <div className="card-body">
-                            <h5 className="card-title">Card title</h5>
-                            <p className="card-text">This is a wider card with supporting text below as a natural 
-        lead-in to additional content. This content is a little bit longer.</p>
-                            <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
-                        </div>
-                    </div>
+            <div className="card-group">
                 <div className="card">
-                    <img src="..." className="card-img-top" alt="..."/>>
-                <div className="card-body">
-                    <h5 className="card-title">Card title</h5>
-                    <p className="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
-                    <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
-                </div>
-                </div>
-                <div className="card">
-                        <img src="..." className="card-img-top" alt="..."/>>
-                        <div className="card-body">
-                            <h5 className="card-title">Card title</h5>
-                            <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
-                            <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div className="row2-container">
-                <div className="card-group">
-                    <div className="card">
-                        <img src="..." className="card-img-top" alt="..."/>>
-                        <div className="card-body">
-                            <h5 className="card-title">Card title</h5>
-                            <p className="card-text">This is a wider card with supporting text below as a natural 
-        lead-in to additional content. This content is a little bit longer.</p>
-                            <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
-                        </div>
-                    </div>
-                <div className="card">
-                    <img src="..." className="card-img-top" alt="..."/>>
-                <div className="card-body">
-                    <h5 className="card-title">Card title</h5>
-                    <p className="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
-                    <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
-                </div>
-                </div>
-                <div className="card">
-                        <img src="..." className="card-img-top" alt="..."/>>
-                        <div className="card-body">
-                            <h5 className="card-title">Card title</h5>
-                            <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
-                            <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div className="row3-container">
-                <div className="card-group">
-                    <div className="card">
-                        <img src="..." className="card-img-top" alt="..."/>>
-                        <div className="card-body">
-                            <h5 className="card-title">Card title</h5>
-                            <p className="card-text">This is a wider card with supporting text below as a natural 
-        lead-in to additional content. This content is a little bit longer.</p>
-                            <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
-                        </div>
-                    </div>
-                <div className="card">
-                    <img src="..." className="card-img-top" alt="..."/>>
-                <div className="card-body">
-                    <h5 className="card-title">Card title</h5>
-                    <p className="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
-                    <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
-                </div>
-                </div>
-                <div className="card">
-                        <img src="..." className="card-img-top" alt="..."/>>
-                        <div className="card-body">
-                            <h5 className="card-title">Card title</h5>
-                            <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
-                            <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div className="row4-container">
-                <div className="card-group">
-                    <div className="card">
-                        <img src="..." className="card-img-top" alt="..."/>>
-                        <div className="card-body">
-                            <h5 className="card-title">Card title</h5>
-                            <p className="card-text">This is a wider card with supporting text below as a natural 
-        lead-in to additional content. This content is a little bit longer.</p>
-                            <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
-                        </div>
-                    </div>
-                <div className="card">
-                    <img src="..." className="card-img-top" alt="..."/>>
-                <div className="card-body">
-                    <h5 className="card-title">Card title</h5>
-                    <p className="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
-                    <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
-                </div>
-                </div>
-                <div className="card">
-                        <img src="..." className="card-img-top" alt="..."/>>
-                        <div className="card-body">
-                            <h5 className="card-title">Card title</h5>
-                            <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
-                            <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
-                        </div>
+                    <img src={props.seinfeldImg} className="card-img-top" alt={props.name}/>>
+                    <div className="card-body">
+                        <p className="card-text"></p>
+                        <p className="card-text"><small className="text-muted"></small></p>
                     </div>
                 </div>
             </div>
