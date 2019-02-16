0ecfc4bcb5bb564b4b3c79e78e2a15fe7f6ce881
Charlie Paull <charliepaull211@gmail.com>
2019-02-16 00:18:09 -0800

    done for day 2/16. 12 cards added.

--- clicky-game/src/App.js
+++ clicky-game/src/App.js
@@ -1,13 +1,15 @@
 import React, {Component} from "react";
 import Nav from "./components/navBar/navBar";
 import Jumbo from "./components/jumbotron/jumbo";
+import Card from "./components/cardLayout/cardLayout";
 
 class App extends Component {
     render() {
         return(
             <div>
                 <Nav />,
-                <Jumbo />
+                <Jumbo />,
+                <Card />
             </div>
         )
     }
