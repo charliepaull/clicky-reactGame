a62c0fcb7f3ae343cedafef757a735cfdfce2cbd
Charlie Paull <charliepaull211@gmail.com>
2019-02-17 13:12:04 -0800

    some css and confirmed shuffle works.

--- clicky-game/src/components/jumbotron/jumbo.js
+++ clicky-game/src/components/jumbotron/jumbo.js
@@ -4,12 +4,9 @@ import "./jumbo.css";
 function Jumbo(props){
     return (
         <div>
-            <div class="jumbotron">
-                <h1 class="display-4">Hello, world!</h1>
-                <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
-        <hr class="my-4"></hr>
-                <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
-                <a class="btn btn-primary btn-lg" href="/" role="button">Learn more</a>
+            <div className="jumbotron">
+                <h1 className="display-4">Seinfeld Clicky Shuffle Game!</h1>
+                <p className="lead">Click on a photo to earn points, but don't click on the same photo twice!</p>
             </div>
         </div>
     )
--- clicky-game/src/components/navBar/navBar.js
+++ clicky-game/src/components/navBar/navBar.js
@@ -4,13 +4,15 @@ import "./nav.css";
 function Nav(props){
     return (
         <div>
-            <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <div className="container">
-                <a className="navbar-brand" href="http://localhost:3000/">Clicky Game</a>
-                <span className="nav-text">Click An Image to Begin</span>
-                <span className="nav-score ">Score: {props.score} || Top Score: {props.topScore}</span>
+            <div className="full-nav">
+                <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                <div className="container">
+                    <a className="navbar-brand" href="http://localhost:3000/">Clicky Game</a>
+                    <span className="nav-text">Click An Image to Begin</span>
+                    <span className="nav-score ">Score: {props.score} || Top Score: {props.topScore}</span>
+                </div>
+                </nav>
             </div>
-            </nav>
         </div>
     )
 };
